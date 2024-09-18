302deec995580e0feec779ef53db4618e436ee7e
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-09-18 13:39:35 +1000

    mutation generation completed

--- snakemake_pipeline/generate_mutations.py
+++ snakemake_pipeline/generate_mutations.py
@@ -5,19 +5,55 @@ import random
 
 
 def calculate_differences(seq1, seq2):
-    positions = []
+    """
+    Calculate the different positions between the two provided sequences, and 
+    identify characters to mutate to at those positions.
+
+    Params: 
+        seq1: SeqRecord object containing the origin sequence
+        seq2: SeqRecord object containing the target sequence
+
+    Returns:
+        List[tuple(int, char)] lsit of tuples (position index, target character)
+    """
+
+    pos_mutation = []
 
     for i, character in enumerate(seq1.seq):
         # print(character, seq2.seq[i])
         if character != seq2.seq[i]:
 
-            positions.append((i, seq2.seq[i]))
+            pos_mutation.append((i, seq2.seq[i]))
 
-    return positions
+    return pos_mutation
+
+
+def get_specified_mutations(seq1, seq2, positions):
+    """
+    Retrieve the characters to mutate to at specific positions, if they differ 
+    from the origin sequence.
+
+    Params: 
+        seq1: SeqRecord object containing the origin sequence
+        seq2: SeqRecord object containing the target sequence
+
+    Returns:
+        List[tuple(int, char)] lsit of tuples (position index, target character)
+    """
+    pos_mutation = []
+
+    for position in positions:
+        if seq1.seq[position] != seq2.seq[position]:
+            pos_mutation.append((position, seq2.seq[position]))
+
+    return pos_mutation
+
+
 
 def generate_mutations(inputfile, outputfile, positions=None, seed=42):
     """
-    Assumes inputfile is fasta of two aligned sequences, first is origin and second is target.
+    Assumes inputfile is fasta of two aligned sequences, 
+    first is origin and second is target.
     """
     random.seed(seed)
 
@@ -30,17 +66,26 @@ def generate_mutations(inputfile, outputfile, positions=None, seed=42):
     if len(origin.seq) != len(target.seq):
         raise ValueError("These sequences are not aligned")
 
-    possible_mutations = calculate_differences(origin, target)
+
+    # calculate allowed characters to mutate to
+    if positions is None:
+        possible_mutations = calculate_differences(origin, target)
+    else:
+        possible_mutations = get_specified_mutations(origin, target, positions)
 
 
+
+    # shuffle the mutations
     random.shuffle(possible_mutations)
 
     
+    # insert first (unmutated) sequence
     record = SeqRecord(
         Seq(str(origin.seq)),
         id=f"{origin.id}_{target.id}_0",
         description=''
     )
+    # store sequence as a mutable for alteration
     mutableseq = MutableSeq(str(origin.seq))
 
     mutated_seqs.append(record)
@@ -48,8 +93,10 @@ def generate_mutations(inputfile, outputfile, positions=None, seed=42):
 
     i = 1
     for pos, mutation in possible_mutations:
+        # mutate the working sequence
         mutableseq[pos] = mutation
 
+        # create a new seqrecord with the mutated sequence
         record = SeqRecord(
             Seq(str(mutableseq)),
             id=f"{origin.id}_{target.id}_{i}",
@@ -57,12 +104,9 @@ def generate_mutations(inputfile, outputfile, positions=None, seed=42):
         )
 
         mutated_seqs.append(record) 
-
         i += 1
 
-
-
-
+    # write all sequences to output file
     SeqIO.write(mutated_seqs, outputfile, 'fasta')
 
 
@@ -72,4 +116,5 @@ def generate_mutations(inputfile, outputfile, positions=None, seed=42):
 # outputfile = snakemake.output
 
 
-generate_mutations('NR1_NR4_ancestors.fasta', 'testoutput.fasta')
+# generate_mutations('NR1_NR4_ancestors.fasta', 'testoutput.fasta')
+generate_mutations('NR1_NR4_ancestors.fasta', 'testoutput.fasta', [0,1,2,3,4])
