989080d2ca5ee8ca703417ea65e98985cb4239f2
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-21 19:01:54 +1000

    started blast parsing

--- snakemake_pipeline/scripts/parse_blast.py
+++ snakemake_pipeline/scripts/parse_blast.py
@@ -9,14 +9,63 @@ def dummy_blast_results(seq_df, output_df):
 
 
 def parse_results(filename):
+
+    results = {}
+
+    USUAL_BLAST_OUTPUTS = {'probable nuclear hormone receptor': 'Other',
+                           'nuclear receptor related 1': 'Other',
+                           'nuclear receptor isoform x1': 'Other',
+                           'nuclear hormone receptor e75': 'Other',
+                           'nuclear receptor of the nerve growth': 'Other',
+                           'ligand-binding domain': 'Other',
+                           'subfamily 2': 'Other',
+
+                           'subfamily 4': 'NR4',
+                           'sub 4': 'NR4', '4 ': 'NR4',
+                           'hr38': 'NR4', ' 38 ': 'NR4',
+                           'hzf-3': 'NR4', 'nurr1': 'NR4',
+                           'nur77': 'NR4', 'nr4': 'NR4',
+                           'nor1': 'NR4', 
+
+                           'subfamily 1': 'NR1', 
+                           'retinoic acid receptor': 'NR1',
+                           'oxysterol': 'NR1', 'LXR': 'NR1',
+                           }
+
     with open(filename) as file:
         for line in file:
             if line.startswith('# Query'):
-                continue
+
+                # get the sequence id from the query line
+                seq_id = line.split()[2]
+
+                # set up seq id in results to track count
+                results[seq_id] = {'NR1':0, 'NR4':0, 'Other':0}
 
             # these lines of the blast results aren't useful here
             elif line.startswith('#') or line == '\n' or line.startswith('Fields') or line.startswith('Processing'):
                 continue
+            
+            # otherwise ilne contains results
+            else:
+                # line is split by tabs
+                fields = line.split('\t')
+
+                # the hit info is index 1 in this search
+                match_info = fields[1]
+                match_info = match_info.lower()
+
+                # check through the common outputs
+                for key in USUAL_BLAST_OUTPUTS.keys():
+                    # if output found in result
+                    if key in match_info:
+                        # increment the appropriate family in results 
+                        results[seq_id][USUAL_BLAST_OUTPUTS[key]] += 1
+                        continue
+
+
+
+
 
 
 def main():
