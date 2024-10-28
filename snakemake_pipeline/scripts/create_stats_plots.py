ee75ff894c813d9f237ea815ca0fe348770922c0
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-28 22:18:03 +1000

    statistics plotting

--- snakemake_pipeline/scripts/create_stats_plots.py
+++ snakemake_pipeline/scripts/create_stats_plots.py
@@ -2,9 +2,8 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def main():
-    input_files = snakemake.input
 
+def extract_mutation_counts(input_files) -> dict:
     # Initialize a dictionary to store mutation counts for each target category
     mutation_counts = {
         "NR1": [],
@@ -43,14 +42,50 @@ def main():
 
     # Reorder mutation counts based on the valid categories order
     ordered_counts = {cat: mutation_counts[cat] for cat in categories_to_track if cat in mutation_counts}
-                
+
+    return ordered_counts
+
+
+def extract_mutated_positions(input_files):
+    all_categories = ["NR1", "NR1-like", "NR4-like", "NR4"]
+    mutated_positions = {category: [] for category in all_categories}
+
+        # Loop through each input file and extract transitions
+    for file in input_files:
+        # Load the data from the file
+        df = pd.read_csv(file)
+
+        # Identify the starting value (first prediction in this file)
+        starting_value = df['overall_prediction'].iloc[0]
+
+        # Define the other categories to track (excluding the starting value)
+        categories_to_track = ["NR1", "NR1-like", "NR4-like", "NR4"]
+        categories_to_track.remove(starting_value)
+        
+        if starting_value == 'NR4':
+            categories_to_track.reverse()
+            mutated_positions.pop('NR4', '')
+        elif starting_value == 'NR1':
+            mutated_positions.pop('NR1', '')
+
+        for category in categories_to_track:
+            transition = df[(df['overall_prediction'].shift() != df['overall_prediction']) &
+                            (df['overall_prediction'] == category)]
+            if not transition.empty:
+                first_transition = transition.iloc[0]
+                mutated_positions[category].extend(first_transition['mutated_positions'])
+
+        ordered_positions = {cat: mutated_positions[cat] for cat in categories_to_track if mutated_positions[cat]}
+        return ordered_positions
+
+
+
+def plot_num_mutations(ordered_counts, output_path):
     # Create a grid of 3 subplots (one for each non-starting category)
     fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
 
-    # print(mutation_counts.items())
     # Plot the frequency for each target category in a separate subplot
     for ax, (category, counts) in zip(axes, ordered_counts.items()):
-        print(f'category is: {category}')
         if counts:  # Only plot if there are relevant transitions
             pd.Series(counts).value_counts().sort_index().plot(kind='bar', color='skyblue', ax=ax)
         ax.set_title(f"Transitions to {category}")
@@ -61,7 +96,42 @@ def main():
     plt.tight_layout()
 
     # Save the plot
-    plt.savefig(snakemake.output.graphs, bbox_inches='tight')
+    plt.savefig(output_path.replace('.png', '_mutation.png'), bbox_inches='tight')
+
+
+def plot_mutated_positions(ordered_positions, output_path):
+    # Create a grid of 3 subplots (one for each non-starting category)
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
+
+    # Plot the frequency for each target category in a separate subplot
+    for ax, (category, counts) in zip(axes, ordered_positions.items()):
+        if counts:  # Only plot if there are relevant transitions
+            pd.Series(counts).value_counts().sort_index().plot(kind='bar', color='skyblue', ax=ax)
+        ax.set_title(f"Transitions to {category}")
+        ax.set_xlabel("Number of Mutations")
+        ax.set_ylabel("Frequency")
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    # Save the plot
+    plt.savefig(output_path.replace('.png', '_positions.png'), bbox_inches='tight')
+
+
+
+
+def main():
+    input_files = snakemake.input
+
+    ordered_counts = extract_mutation_counts(input_files)
+    plot_num_mutations(ordered_counts, snakemake.output.graphs)
+
+    ordered_positions = extract_mutated_positions(input_files)
+    plot_mutated_positions(ordered_positions, snakemake.output.graphs)
+
+
+                
+    
 
     
 
