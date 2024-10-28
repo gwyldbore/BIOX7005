40601f63ca3e81e1efd745825f9a98daffbea01b
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-28 18:55:46 +1000

    creating prediction pca plots

--- snakemake_pipeline/scripts/plot_pca.py
+++ snakemake_pipeline/scripts/plot_pca.py
@@ -60,11 +60,74 @@ def plot_pca(all_embeddings_df, nodes_to_label, outpath, col_name='protbert_cls_
     plt.legend()
     plt.savefig(outpath)
 
+def plot_pca_colour_by_predicted(all_embeddings_df, nodes_to_label, outpath, col_name='protbert_cls_embedding'):
+
+    embeddings = np.vstack(all_embeddings_df[col_name].values)
+
+
+    # Apply PCA
+    num_components = 2
+    pca = PCA(n_components=num_components)
+    pca_result = pca.fit_transform(embeddings)
+
+    # Add PCA results to the DataFrame
+    all_embeddings_df['pca1'] = pca_result[:, 0]
+    all_embeddings_df['pca2'] = pca_result[:, 1]
+
+    # Get unique clades
+    clades_with_color = all_embeddings_df['Clade'].dropna().unique()
+    num_clades = len(clades_with_color)
+
+    # Define color map for the clades
+    colors = plt.cm.get_cmap('Set1', num_clades).colors
+
+    plt.figure(figsize=(20, 14))
+
+    # Plot all points in gray first to show entries with no clade
+    no_clade_df = all_embeddings_df[all_embeddings_df['Clade'].isna()]
+    # probably a way to set colour=blue to be a gradient??
+    plt.scatter(no_clade_df['pca1'], no_clade_df['pca2'], color='blue', alpha=0.5, label='No Clade')
+
+    # Plot points with clades in different colors
+    for clade, color in zip(clades_with_color, colors):
+        subset = all_embeddings_df[all_embeddings_df['Clade'] == clade]
+        plt.scatter(subset['pca1'], subset['pca2'], label=clade, color=color)
+
+
+
+    # Overlay predictions with new colors
+    prediction_df = all_embeddings_df.dropna(subset=['overall_prediction'])
+    unique_predictions = prediction_df['overall_prediction'].unique()
+
+    # Define a new colormap for predictions
+    prediction_colors = plt.cm.get_cmap('cool', len(unique_predictions)).colors
+
+    for prediction, color in zip(unique_predictions, prediction_colors):
+        pred_subset = prediction_df[prediction_df['overall_prediction'] == prediction]
+        plt.scatter(pred_subset['pca1'], pred_subset['pca2'], 
+                    color=color, marker='x', s=100, alpha=0.8, 
+                    label=f'Prediction: {prediction}')
+
+
+
+    # Set plot titles and labels
+    plt.title("PCA by Clade")
+    plt.xlabel("PCA Component 1")
+    plt.ylabel("PCA Component 2")
+    plt.legend()
+    plt.savefig(outpath)
+
 
 def main():
     # Load the df with the mutated sequences
     with open(snakemake.input.embedding_df, "rb") as input_file:
         embedding_df = pickle.load(input_file)
+
+    # load the df with the prediction info
+    with open(snakemake.input.predictions_df, "rb") as prediction_input:
+        prediction_df = pickle.load(prediction_input)
+    embedding_predictions = pd.merge(embedding_df, prediction_df[['info', 'overall_prediction']], 
+                                     on='info', how='left')
 
     nodes_to_label = embedding_df['info'].values
     # print('Nodes to label:', nodes_to_label)
@@ -84,8 +147,11 @@ def main():
     all_embeddings_df = pd.concat([embedding_df, specific_ancestor_embedding_df])
 
     # Plot PCA
-    plot_pca(all_embeddings_df, nodes_to_label, snakemake.output.plot)
+    plot_pca(all_embeddings_df, nodes_to_label, snakemake.output.plot_mutation)
 
+
+    all_embeddings_prediction_df = pd.concat([embedding_predictions, specific_ancestor_embedding_df])
+    plot_pca_colour_by_predicted(all_embeddings_prediction_df, nodes_to_label, snakemake.output.plot_prediction)
 
 if __name__ == "__main__":
     main()
