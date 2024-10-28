1eacf3bf6b942908761d53a60b4e59c0dce5011a
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-29 01:38:02 +1000

    fixing more pca colour

--- snakemake_pipeline/scripts/plot_pca.py
+++ snakemake_pipeline/scripts/plot_pca.py
@@ -131,7 +131,7 @@ def plot_pca_colour_by_predicted(all_embeddings_df, nodes_to_label, outpath, col
     # prediction_colors = plt.colormaps['PiYG'].resampled(len(unique_predictions))
 
     # prediction_cmap = ListedColormap(['cyan', 'chartreuse', 'hotpink', 'blueviolet'])
-    prediction_cmap = ListedColormap(['#e41a1c', '#377eb8', '#984ea3', '#4d4d4d'])
+    prediction_cmap = ListedColormap(['#ff7c0a', 'red', 'royalblue', 'limegreen'])
     prediction_colors = plt.get_cmap(prediction_cmap, len(unique_predictions)).colors
 
     for prediction, color in zip(unique_predictions, prediction_colors):
