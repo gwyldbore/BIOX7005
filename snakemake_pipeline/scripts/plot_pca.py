1eee27a50cdd456d8c9a07d862022152d8826d34
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-29 00:54:26 +1000

    fixing more pca colour

--- snakemake_pipeline/scripts/plot_pca.py
+++ snakemake_pipeline/scripts/plot_pca.py
@@ -28,7 +28,8 @@ def plot_pca(all_embeddings_df, nodes_to_label, outpath, col_name='protbert_cls_
     # Define color map for the clades
     # colors = plt.cm.get_cmap('Set1', num_clades).colors
     # colors = plt.colormaps['PiYG'].resampled(num_clades)
-    clade_cmap = ListedColormap(['orangered', 'goldenrod'])
+    # clade_cmap = ListedColormap(['orangered', 'goldenrod'])
+    clade_cmap = ListedColormap(['green', 'royalblue'])
     colors = plt.get_cmap(clade_cmap, num_clades).colors
 
     # plt.figure(figsize=(20, 14))
