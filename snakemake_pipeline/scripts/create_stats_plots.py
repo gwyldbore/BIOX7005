b64551ace958bbcbed0febbbf5e2a337ecc1075c
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-28 21:31:53 +1000

    statistics plotting

--- snakemake_pipeline/scripts/create_stats_plots.py
+++ snakemake_pipeline/scripts/create_stats_plots.py
@@ -18,15 +18,15 @@ def main():
         # Load the data from the file
         df = pd.read_csv(file)
 
-        # Sort by 'info' to ensure the transitions are in correct order
-        df = df.sort_values('info')
-
         # Identify the starting value (first prediction in this file)
         starting_value = df['overall_prediction'].iloc[0]
 
         # Define the other categories to track (excluding the starting value)
         categories_to_track = ["NR1", "NR1-like", "NR4-like", "NR4"]
         categories_to_track.remove(starting_value)
+
+        if starting_value == 'NR4':
+            categories_to_track = categories_to_track.reverse()
 
         # Track the first transition to each non-starting category
         for category in categories_to_track:
@@ -52,11 +52,6 @@ def main():
 
     # Adjust layout to prevent overlap
     plt.tight_layout()
-
-    # Add labels and title
-    plt.title(f"Frequency of 'num_mutation' Changes")
-    plt.xlabel("Number of Mutations")
-    plt.ylabel("Frequency")
 
     # Save the plot
     plt.savefig(snakemake.output.graphs, bbox_inches='tight')
