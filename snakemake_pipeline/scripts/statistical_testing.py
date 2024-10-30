192435a84633c46e8f041d5d99f92cba8910f6ec
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-30 18:13:06 +1000

    doing some stats

---
+++ snakemake_pipeline/scripts/statistical_testing.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+import scikit_posthocs as sp
+
+
+
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='Method', y='Mutation_Count', data=df)
+plt.title('Mutation Counts by Method')
+plt.savefig('boxplot.png')
+plt.close()
+
+
+
+def main():
+    input_files = snakemake.input
+    method_name = snakemake.wildcards.method_name
+
+    replicates = [pd.read_csv(file) for file in input_files]
+    combined_df = pd.concat(replicates, ignore_index=True)
+
+
+    # make a boxplot of the stats
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='Method', y='Mutation_Count', data=combined_df)
+    plt.title(f'Mutation Counts by {method_name}')
+    plt.savefig(snakemake.output.boxplot)
+    plt.close() # close to save memory
+
+    
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
