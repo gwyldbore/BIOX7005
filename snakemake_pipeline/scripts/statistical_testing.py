98038917d1029a92da50f572ac604946a70cb820
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-30 18:22:58 +1000

    doing some stats

--- snakemake_pipeline/scripts/statistical_testing.py
+++ snakemake_pipeline/scripts/statistical_testing.py
@@ -22,8 +22,45 @@ def main():
     plt.figure(figsize=(10, 6))
     sns.boxplot(x='method', y='num_mutation', data=combined_df)
     plt.title(f'Mutation Counts by {method_name}')
+    plt.ylabel("Number of mutations")
     plt.savefig(snakemake.output.boxplot)
     plt.close() # close to save memory
+
+
+
+    # 3.1 Shapiro-Wilk test for normality
+    for method, data in combined_df.groupby('Method'):
+        stat, p = stats.shapiro(data['Mutation_Count'])
+        print(f'{method} - Shapiro-Wilk test p-value: {p}')
+
+    # 3.2 Levene’s test for equal variances
+    stat, p = stats.levene(
+        *[group['Mutation_Count'].values for name, group in combined_df.groupby('Method')]
+    )
+    print(f'Levene’s test p-value: {p}')
+
+    # 3.3 Hypothesis Testing (ANOVA or Kruskal-Wallis)
+    if p > 0.05:  # If variances are equal
+        stat, p = stats.f_oneway(
+            *[group['Mutation_Count'].values for name, group in combined_df.groupby('Method')]
+        )
+        print(f'ANOVA test p-value: {p}')
+        if p < 0.05:
+            tukey = pairwise_tukeyhsd(combined_df['Mutation_Count'], combined_df['Method'])
+            print(tukey)
+    else:
+        stat, p = stats.kruskal(
+            *[group['Mutation_Count'].values for name, group in combined_df.groupby('Method')]
+        )
+        print(f'Kruskal-Wallis test p-value: {p}')
+        if p < 0.05:
+            dunn = sp.posthoc_dunn(combined_df, val_col='Mutation_Count', group_col='Method', p_adjust='bonferroni')
+            print(dunn)
+
+    # Step 4: Chi-square Test for Mutated Positions
+    positions_df = pd.crosstab(combined_df['Position'], combined_df['Method'])
+    chi2, p, _, _ = stats.chi2_contingency(positions_df)
+    print(f'Chi-square test p-value: {p}')
 
 
 
