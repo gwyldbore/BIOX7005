578d6a28d0e6728fdfe6b3dda097909e753222aa
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-10-30 13:59:14 +1000

    testing pca grouping again

--- snakemake_pipeline/scripts/pcatest.py
+++ snakemake_pipeline/scripts/pcatest.py
@@ -48,18 +48,21 @@ def plot_pca_ancestors_static(mutations_df, ancestors_df, nodes_to_label, outpat
                 4: '^',
                 5: 's'}
     
-    
+
     mutation_df = mutations_df.dropna(subset=['num_mutation'])
-    # scatter = ax.scatter(mutation_df['pca1'], mutation_df['pca2'], 
-    #             c=[int(x) for x in mutation_df['num_mutation']], cmap='cool')
+    scatter = ax.scatter(mutation_df['pca1'], mutation_df['pca2'], 
+                c=[int(x) for x in mutation_df['num_mutation']], cmap='cool', 
+                marker=marker_dict[int(mutation_df['filegroup'])])
+    
+    
     # Plot points with different markers based on 'filegroup' column
-    for group, marker in marker_dict.items():
-        group_subset = mutation_df[mutation_df['filegroup'] == group]
-        scatter = ax.scatter(
-            group_subset['pca1'], group_subset['pca2'],
-            c=[int(x) for x in group_subset['num_mutation']],
-            cmap='cool', marker=marker, alpha=0.8, label=group
-        )
+    # for group, marker in marker_dict.items():
+    #     group_subset = mutation_df[mutation_df['filegroup'] == group]
+    #     scatter = ax.scatter(
+    #         group_subset['pca1'], group_subset['pca2'],
+    #         c=[int(x) for x in group_subset['num_mutation']],
+    #         cmap='cool', marker=marker, alpha=0.8, label=group
+    #     )
     
     cax = ax.inset_axes([0.05, 0.05, 0.3, 0.05])
     fig.colorbar(scatter, cax=cax, orientation='horizontal')
