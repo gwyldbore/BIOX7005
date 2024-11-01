578a738da6d440cb109782ce39ddedfa3dcc1f7c
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-11-01 17:54:16 +1000

    plotting pca with more nacestors

--- snakemake_pipeline/scripts/plot_pca.py
+++ snakemake_pipeline/scripts/plot_pca.py
@@ -82,8 +82,8 @@ def plot_pca_ancestors_static(mutations_df, ancestors_df, nodes_to_label, outpat
     
     ancestor_embeddings = np.vstack(ancestors_df[col_name].values)
 
-    # pca = PCA(n_components=2)
-    pca = PCA(n_components=10)
+    pca = PCA(n_components=2)
+    # pca = PCA(n_components=10)
     pca_result = pca.fit(ancestor_embeddings)
 
     explained_variance_ratio = pca.explained_variance_ratio_
@@ -298,8 +298,8 @@ def main():
     ancestor_embedding_df['Clade'] = ancestor_embedding_df['info'].apply(seq_utils.tag_node, dataset='combined')
 
     # Filter for only NR1 or NR4 clades
-    # specific_ancestor_embedding_df = ancestor_embedding_df[ancestor_embedding_df['Clade'].isin(['NR1', 'NR4'])]
-    specific_ancestor_embedding_df = ancestor_embedding_df
+    specific_ancestor_embedding_df = ancestor_embedding_df[ancestor_embedding_df['Clade'].isin(['NR1', 'NR4'])]
+    # specific_ancestor_embedding_df = ancestor_embedding_df
 
 
     # Concatenate the embeddings and ancestor embeddings
