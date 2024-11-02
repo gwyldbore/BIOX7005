3295343bdde5944d2bad67b2c4861bd49c83dd36
gwyldbore <georgia.wyldbore@uqconnect.edu.au>
2024-11-02 21:33:37 +1000

    new plots

--- snakemake_pipeline/scripts/plot_pca_grouped.py
+++ snakemake_pipeline/scripts/plot_pca_grouped.py
@@ -112,32 +112,31 @@ def plot_pca_colour_by_predicted_ancestors_static(mutations_df, ancestors_df, no
     unique_predictions = prediction_df['overall_prediction'].unique()
 
     # define new cmap
-    prediction_cmap = ListedColormap(['mediumorchid', 'red', 'royalblue', 'forestgreen'])
+    prediction_cmap = ListedColormap(['darkorchid', 'forestgreen', 'magenta', 'firebrick',  'royalblue'])
     if unique_predictions[0] == 'NR4':
-        prediction_cmap = ListedColormap(['forestgreen', 'royalblue', 'red', 'mediumorchid'])
+        prediction_cmap = ListedColormap(['royalblue', 'firebrick', 'magenta', 'forestgreen', 'darkorchid'])
 
     prediction_colors = plt.get_cmap(prediction_cmap, len(unique_predictions)).colors
 
-    # for prediction, color in zip(unique_predictions, prediction_colors):
-    #     pred_subset = prediction_df[prediction_df['overall_prediction'] == prediction]
-    #     plt.scatter(pred_subset['pca1'], pred_subset['pca2'], 
-    #                 color=color, label=f'Prediction: {prediction}',
-    #                 marker=marker_dict[prediction_df['filegroup']])
-
-        # Plot predictions grouped by prediction type and filegroup
     for prediction, color in zip(unique_predictions, prediction_colors):
         pred_subset = prediction_df[prediction_df['overall_prediction'] == prediction]
+        plt.scatter(pred_subset['pca1'], pred_subset['pca2'], 
+                    color=color, label=f'Prediction: {prediction}')
 
-        # Further group by 'filegroup' to assign different markers
-        for group, marker in marker_dict.items():
-            group_subset = pred_subset[pred_subset['filegroup'] == group]
+        # Plot predictions grouped by prediction type and filegroup
+    # for prediction, color in zip(unique_predictions, prediction_colors):
+    #     pred_subset = prediction_df[prediction_df['overall_prediction'] == prediction]
 
-            if not group_subset.empty:  # Plot only if there are points in this group
-                ax.scatter(
-                    group_subset['pca1'], group_subset['pca2'], 
-                    color=color, label=f'{prediction} - Group {group}',
-                    marker=marker, alpha=0.8
-                )
+    #     # Further group by 'filegroup' to assign different markers
+    #     for group, marker in marker_dict.items():
+    #         group_subset = pred_subset[pred_subset['filegroup'] == group]
+
+    #         if not group_subset.empty:  # Plot only if there are points in this group
+    #             ax.scatter(
+    #                 group_subset['pca1'], group_subset['pca2'], 
+    #                 color=color, label=f'{prediction} - Group {group}',
+    #                 marker=marker, alpha=0.8
+    #             )
 
     # Set plot titles and labels
     plt.title("PCA by Clade")
